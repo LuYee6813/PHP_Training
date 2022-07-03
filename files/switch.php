2b96a02f3cf9861cbe307a5d55948a56b3dd1692
AnonymousLu <42244975+LuYee6813@users.noreply.github.com>
2022-07-03 22:36:45 +0800

    update

--- files/switch.php
+++ files/switch.php
@@ -8,8 +8,18 @@
 </head>
 <body>
     <form action="switch.php" method="get" >
-        name: <input type="text" name="name">
+        enter key: <input type="text" name="key">
     </form>
-    
+    <?php
+        $key = $_GET['key'];
+        switch ($key) 
+        {
+            case 'flag':
+                echo "flag{You C4n 533 M3!}";
+                break;
+            default:
+                echo "wrong key";
+        }
+    ?>
 </body>
 </html>
