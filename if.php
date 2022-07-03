b543ee69e8dfd17af47bd62c70edade10c447880
AnonymousLu <42244975+LuYee6813@users.noreply.github.com>
2022-07-03 19:57:59 +0800

    update

---
+++ if.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>IF</title>
+</head>
+<body>
+    <form action="if.php" method="post">
+        num1:<input type="text" name="num1">
+    </form>
+    <?php
+        $num1 = $_POST['num1'];
+        $num2 = 5;
+        if( $num1 > $num2){
+            echo "you num1->{$num1} is big than num2->{$num2}";
+        }
+        else{
+            echo "you num1->{$num1} is small than num2->{$num2}";
+        }
+    ?>
+</body>
+</html>
--- switch.php
+++ switch.php
@@ -4,17 +4,9 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>function</title>
+    <title>switch</title>
 </head>
 <body>
-    <?php
-        function sayHi($name){
-            echo "<h1>Hi $name</h1>";
-        }
-        sayHi("Ivan");
-        sayHi("Andy");
-        sayHi("John");
-    ?>
-
+    
 </body>
 </html>
